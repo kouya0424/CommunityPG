d42b0094e1b94e3f11e61b5f94ba2ad2f9a4f522
kouya0424 <ajimabass@gmail.com>
2021-10-01 07:26:40 +0900

    ディレクトリパスの修正

--- src/main/java/com/rugbyaholic/communityPG/manage/sys/fs/DirectoryService.java
+++ src/main/java/com/rugbyaholic/communityPG/manage/sys/fs/DirectoryService.java
@@ -12,7 +12,7 @@ import com.rugbyaholic.communityPG.manage.sys.fs.upload.UploadForm;
 public class DirectoryService {
 	
 	public String homeDirectory() {
-		return "Share";
+		return "./src/main/resources/com/rugbyaholic/communityPG/Share";
 	}
 	
 	public void makeDirectory(String currentDirectory, String folderName) {
